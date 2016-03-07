5cb02d260631c06c7f398f47f4bc6467dfe8279f
Zhihao Yang <yang_zhihao@live.com>
2016-03-07 14:40:56 +0100

    edit to the controller

--- app/js/dinnerCtrl.js
+++ app/js/dinnerCtrl.js
@@ -14,5 +14,16 @@ dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
 
   // TODO in Lab 5: Implement the methods to get the dinner menu
   // add dish to menu and get total menu price
+  $scope.getDinnerMenu = function() {
+  	return Dinner.menu;
+  }
+
+  $scope.addDishToMenu = function(dish) {
+  	Dinner.addDishToMenu(dish);
+  }
+
+  $scope.getTotalMenuPrice = function() {
+  	return Dinner.getTotalMenuPrice();
+  }
 
 });
